c5c01715d244da5bf0722807cfb6e75bafb83fce
sridevi-ch <52571028+sridevi-ch@users.noreply.github.com>
2019-07-16 17:43:58 +0530

    Update TelemetryService.js

--- src/TelemetryService.js
+++ src/TelemetryService.js
@@ -8,7 +8,7 @@ class TelemetryService {
     }
 
     initialize(reactPluginConfig) {
-        let INSTRUMENTATION_KEY = 'YOUR_IKEY'; // Enter your instrumentation key here
+        let INSTRUMENTATION_KEY = '3c59fa15-2efb-4ed8-8494-1243c08074fa'; // Enter your instrumentation key here
         
         this.appInsights = new ApplicationInsights({
             config: {
